898348d86c53703b0d70a1e6bdeaf1684d4f7c2a
Konstantin <levachin@luckandroll.com>
2020-06-28 14:23:10 +0300

    added simple routing

--- app/src/components/layout/Layout.js
+++ app/src/components/layout/Layout.js
@@ -1,9 +1,13 @@
 import React, {Fragment} from 'react'
+import {Route, Switch} from 'react-router-dom'
 import './layout.scss'
 import {AddClass} from "../../hoc/AddClass";
 import {Header} from "../header/Header";
 import {Home} from "../../pages/home/Home";
 import {Sidebar} from "../sidebar/Sidebar";
+import {Calc} from "../../pages/calc/Calc";
+import {Sample} from "../../pages/sample/Sample";
+import {Info} from "../../pages/info/Info";
 
 const Layout = () => {
     return(
@@ -11,7 +15,12 @@ const Layout = () => {
             <Header />
             <div className='content'>
                 <div className='routes'>
-                    <Home />
+                    <Switch>
+                        <Route path='/' exact component = {Home} />
+                        <Route path='/calc' render={() => <Calc />} />
+                        <Route path='/sample' component = {Sample} />
+                        <Route path='/info' component = {Info} />
+                    </Switch>
                 </div>
                 <Sidebar />
             </div>
---
+++ app/src/pages/calc/Calc.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './calc.scss'
+
+export const Calc = () => {
+    return(
+        <h1>Calc page</h1>
+    )
+}
